dedd2653b9cf283818344ebda9e95032efccf568
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-28 11:59:10 +0700

    feat: added addPeer and removePeer service methods

--- src/services/groups.ts
+++ src/services/groups.ts
@@ -1,5 +1,6 @@
 import { validate as uuidValidate } from "uuid";
 import { GROUPS } from "../server";
+import { WebSocketEventDataDesc } from "../types";
 
 const getGroupById = (groupId: string) => {
   if (uuidValidate(groupId)) {
@@ -17,7 +18,51 @@ const isExistedPeer = (groupId: string, peerId: string): string | undefined => {
   return group.peerIds.find((element) => element === peerId);
 };
 
+const addPeer = (
+  groupId: string,
+  peerId: string,
+  desc: WebSocketEventDataDesc
+) => {
+  let group = GROUPS[groupId];
+  if (group) {
+    group.peerIds.push(peerId);
+    const mixedDesc = { ...desc, ...group.desc };
+    group.desc = mixedDesc;
+  }
+
+  return { ...group };
+};
+
+const removePeer = (
+  groupId: string,
+  peerId: string
+): { peers: string[]; host: string } => {
+  if (!GROUPS[groupId]) return { peers: [], host: "" };
+
+  const group = GROUPS[groupId];
+  if (!group) return { peers: [], host: "" };
+
+  const peerIds = group.peerIds;
+  const peerIdsAfterRemove = peerIds.filter((element) => element !== peerId);
+
+  group.peerIds = peerIdsAfterRemove;
+
+  if (peerId === group.host) {
+    let firstPeer = peerIdsAfterRemove[0];
+    if (!firstPeer) return { peers: [], host: "" };
+    group.host = firstPeer;
+  }
+
+  if (peerIdsAfterRemove.length === 0) {
+    delete GROUPS[groupId];
+  }
+
+  return { peers: [...peerIdsAfterRemove], host: group.host };
+};
+
 export const groupServices = {
   getGroupById,
   isExistedPeer,
+  addPeer,
+  removePeer,
 };
