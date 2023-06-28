20da60b32fcf47bc9a131a2aa5727fd2c7eedbcf
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-28 11:06:41 +0700

    feat: commit config and constants

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const enum SIGNALING_MESSAGE_TYPES {
+  ONLINE = "online",
+  OFFER = "offer",
+  ANSWER = "answer",
+  CANDIDATE = "candidate",
+  JOIN_GROUP = "join_group",
+  NEW_PEER = "new_peer",
+  LEAVE_GROUP = "leave_group",
+}
