e8d0eb584c6dbc2093bb0069c1e08930b37383df
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-28 11:09:01 +0700

    feat: added offer handler

---
+++ src/handler/offer.ts
@@ -0,0 +1,22 @@
+import { sendMessage } from ".";
+import { SIGNALING_MESSAGE_TYPES } from "../constants";
+import { CONNS, PEERS } from "../server";
+import { groupServices } from "../services";
+import { WebSocketEventData } from "../types";
+
+export const handleOffer = (data: WebSocketEventData): void => {
+  const { groupId, senderId, receiverId, offer } = data;
+  if (!groupId || !senderId || !receiverId || !offer) return;
+
+  const peerId = groupServices.isExistedPeer(groupId, receiverId);
+  console.log("Handle offer :", groupId, receiverId, peerId);
+  if (!peerId) return;
+
+  let peer = PEERS[peerId];
+  if (!peer) return;
+
+  sendMessage(CONNS[peer.connId], SIGNALING_MESSAGE_TYPES.OFFER, {
+    senderId,
+    offer,
+  });
+};
