4197c1d963cf0e4e1b4ea32d6783b4f7b3331d8c
Andrew Giang <nguyendtgiang@gmail.com>
2023-06-30 10:07:03 +0700

    feat: added `getLocation` to ipService

--- src/services/ip.ts
+++ src/services/ip.ts
@@ -1,11 +1,34 @@
+import to from "await-to-js";
+import axios from "axios";
+
+import { IPLOCATION_URL } from "../config";
+import { LocationResponse } from "../types";
+
 const ip4v2 = (ipAddress: string): string => {
   const comps = ipAddress.replace(/::/g, "").split(":");
-  if (comps[1] === undefined || comps[0] === undefined) {
+  if (!comps[1] || !comps[0]) {
     throw new Error("The provided input was not an ip address: " + ipAddress);
   }
   return comps[1] || comps[0];
 };
 
+const getLocation = async (ip: string): Promise<LocationResponse> => {
+  const [err, result] = await to(
+    axios.get<LocationResponse>(`${IPLOCATION_URL}/query?ip=${ip}`)
+  );
+  if (err) {
+    return {
+      data: {
+        data: {
+          ISP: "unknown",
+        },
+      },
+    };
+  }
+  return result.data;
+};
+
 export const ipService = {
   ip4v2,
+  getLocation,
 };
