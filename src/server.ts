d33a087ce17a77ec58e05826f64c2c1dfd94c82f
Andrew Giang <nguyendtgiang@gmail.com>
2023-07-03 15:02:50 +0700

    refactor: made some small changes to server.ts
    1. Added comments on what the value format the ipAddress variable should hold
    2. Group 3 variables groupId, peerId and connId into an object called globalIds and it is of type GlobalIds, in order to pass it around as reference
    3. Remove `+ ipAddress` in the Error response since ipAddress in this case is of type string[]

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,13 @@ import { SIGNALING_MESSAGE_TYPES } from "./constants";
 import { handleMessage } from "./handler";
 import { sendMessage } from "./handler/message";
 import { groupServices, ipService, verifier } from "./services";
-import { CONNS_TYPE, GROUPS_TYPE, PEERS_TYPE, WebSocketEvent } from "./types";
+import {
+  CONNS_TYPE,
+  GROUPS_TYPE,
+  GlobalIds,
+  PEERS_TYPE,
+  WebSocketEvent,
+} from "./types";
 
 dotenv.config();
 
@@ -19,6 +25,10 @@ const wss = new WebSocketServer({
 });
 
 wss.on("connection", (ws, req) => {
+  /* 
+    The ipAddress variable should have a value of this format: 
+    ::ffff:1.2.3.4
+  */
   let ipAddress = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
   if (!ipAddress) {
     throw new Error(
@@ -27,14 +37,16 @@ wss.on("connection", (ws, req) => {
   } else if (Array.isArray(ipAddress) && typeof ipAddress[0] === "string") {
     ipAddress = ipAddress[0];
   } else if (typeof ipAddress !== "string") {
-    throw new Error("IP address was not a string: " + ipAddress);
+    throw new Error("IP address was not a string");
   }
 
   const ip = ipService.ip4v2(ipAddress);
 
-  let groupId: string;
-  let peerId: string;
-  let connId: string;
+  let globalIds: GlobalIds = {
+    groupId: "",
+    peerId: "",
+    connId: "",
+  };
 
   ws.on("message", async (event: string) => {
     console.log("--------------------------------------------");
@@ -43,18 +55,23 @@ wss.on("connection", (ws, req) => {
     console.log("Data :", allowedReq, type, data);
     if (!allowedReq) return;
 
-    await handleMessage(ws, type, data, ip, peerId, connId);
+    handleMessage(ws, type, data, ip, globalIds);
+    console.log("GROUPS: ", GROUPS);
+    console.log("PEERS: ", PEERS);
   });
 
   const close = () => {
-    if (!PEERS[peerId]) return;
-    const peer = PEERS[peerId];
+    if (!PEERS[globalIds.peerId]) return;
+    const peer = PEERS[globalIds.peerId];
     if (!peer) return;
     const { connId } = peer;
     delete CONNS[connId];
-    delete PEERS[peerId];
+    delete PEERS[globalIds.peerId];
 
-    const { peers = [], host } = groupServices.removePeer(groupId, peerId);
+    const { peers = [], host } = groupServices.removePeer(
+      globalIds.groupId,
+      globalIds.peerId
+    );
     const updatedPeerIds = [...peers];
     updatedPeerIds.forEach((peerId) => {
       const peer = PEERS[peerId];
